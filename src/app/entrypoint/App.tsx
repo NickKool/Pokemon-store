81bbf6e9dc03c122cd7ebed300c1ab69af591c08
Nikolay <nickcooluxui@gmail.com>
2026-05-02 03:55:35 +0300

    feat: add global styles and tailwind configuration

---
+++ src/app/entrypoint/App.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import '@/app/styles/index.css';
+
+export default class App extends Component {
+  render() {
+    return <div className=" flex flex-col items-center">svsvsev</div>;
+  }
+}
