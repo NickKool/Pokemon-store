8cb5a3b4a50d11af30a55e209bbedcfbada1c125
Nikolay <nickcooluxui@gmail.com>
2026-05-10 16:40:43 +0300

    fix: fix test for button

--- src/shared/ui/button/Button.test.tsx
+++ src/shared/ui/button/Button.test.tsx
@@ -10,27 +10,27 @@ describe('Button component', () => {
 
   it('should the disabled attribute if isLoading={true}', () => {
     render(<Button isLoading={true}>Find</Button>);
-    const buttonElement = screen.getByRole('button');
+    const buttonElement = screen.getByRole('button',{name:/find/i});
     expect(buttonElement).toBeDisabled();
   });
 
   it('should not be disabled if isLoading={false}', () => {
     render(<Button isLoading={false}>Find</Button>);
-    const buttonElement = screen.getByRole('button');
+    const buttonElement = screen.getByRole('button',{name:/find/i});
     expect(buttonElement).not.toBeDisabled();
   });
 
   it('should combine base styles with an additional className', () => {
     const customClass = 'extra-margin';
     render(<Button className={customClass}>Find</Button>);
-    const buttonElement = screen.getByRole('button');
+    const buttonElement = screen.getByRole('button',{name:/find/i});
     expect(buttonElement).toHaveClass(customClass);
     expect(buttonElement).toHaveClass('bg-btn-bg');
   });
 
   it('should pass standard attributes such as type', () => {
     render(<Button type="submit">Find</Button>);
-    const buttonElement = screen.getByRole('button');
+    const buttonElement = screen.getByRole('button',{name:/find/i});
     expect(buttonElement).toHaveAttribute('type', 'submit');
   });
 });
