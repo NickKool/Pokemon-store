47de5f5f9dd5ecca439d1719caf0856a4b955dc9
Nikolay <nickcooluxui@gmail.com>
2026-05-24 13:15:38 +0300

    feat: create TanStack Query hooks for list, search and cache refresh

--- src/entities/pokemon/index.ts
+++ src/entities/pokemon/index.ts
@@ -1,2 +1,7 @@
 export { PokemonCard } from '@/entities/pokemon/ui/pokemon-card/PokemonCard';
 export { PokemonApi } from '@/entities/pokemon/api/PokemonApi';
+export {
+  usePokemonList,
+  usePokemonByName,
+  useRefreshPokemons,
+} from '@/entities/pokemon/api/pokemonQueries';
